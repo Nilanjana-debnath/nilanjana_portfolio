d0627963c9e481d5fc5d06eb3a06b1d1e2293008
Nilanjana-debnath <nilanjana.debnath24@gmail.com>
2025-03-12 18:13:50 +0530

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import ChatToggle from "./ChatToggle";
 import ThemeToggle from "./ThemeToggle";
 // import AIChatButton from "./AIChatButton";
 import { Home, Briefcase, Contact, BookOpen, Box, icons, Boxes } from "lucide-react";
+
 const navLinks = [
   {
     name: "Home",
@@ -28,19 +29,19 @@ const navLinks = [
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-background/75 py-6 backdrop-blur-sm">
-      <nav className="flex items-center justify-between">
-        <ul className="flex gap-4 sm:gap-8">
+    <header className="sticky top-0 z-50 bg-background/75 py-4 sm:py-6 backdrop-blur-sm">
+      <nav className="flex flex-col sm:flex-row items-center justify-between px-4 sm:px-6">
+        <ul className="flex flex-col sm:flex-row gap-2 sm:gap-4 md:gap-8">
           {navLinks.map((nav, id) => (
             <li key={id} className="link">
-              <Link href={nav.href} className="relative px-3 py-2 rounded-md text-sm font-medium   group flex items-center"
-              >
-                  <nav.icon className="w-5 h-5 mr-2" />
-              {nav.name}</Link>
+              <Link href={nav.href} className="relative px-3 py-2 rounded-md text-sm font-medium group flex items-center">
+                <nav.icon className="w-5 h-5 mr-2" />
+                {nav.name}
+              </Link>
             </li>
           ))}
         </ul>
-        <div className="flex gap-0 sm:gap-4">
+        <div className="flex gap-2 sm:gap-4 mt-4 sm:mt-0">
           <ChatToggle />
           {/* <AIChatButton/> */}
           <ThemeToggle />
