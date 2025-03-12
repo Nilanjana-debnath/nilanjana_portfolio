d2e4521711cb6df68357a983708bafdd68432498
Nilanjana-debnath <nilanjana.debnath24@gmail.com>
2025-03-12 16:58:06 +0530

    commit

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -21,9 +21,11 @@ export function ProjectCard({ project }: Props) {
 
   return (
     <Link href={href || `/projects/${name.toLowerCase().replace(/\s+/g, '-')}`} passHref>
-      <Card className="flex flex-col cursor-pointer">
-        <CardHeader>
-          {image && (
+    console.log(href);
+    <Card className="flex flex-col">
+      <CardHeader>
+        {image && (
+          <Link href={href || image}>
             <Image
               src={image}
               alt={name}
@@ -31,42 +33,43 @@ export function ProjectCard({ project }: Props) {
               height={300}
               className="h-40 w-full object-cover object-top"
             />
-          )}
-        </CardHeader>
-        <CardContent className="flex flex-col gap-2">
-          <CardTitle>{name}</CardTitle>
-          <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
-            {description}
-          </Markdown>
-        </CardContent>
-        <CardFooter className="flex h-full flex-col items-start justify-between gap-4">
-          {tags && tags.length > 0 && (
-            <div className="mt-2 flex flex-wrap gap-1">
-              {tags.toSorted().map((tag) => (
-                <Badge
-                  key={tag}
-                  className="px-1 py-0 text-[10px]"
-                  variant="secondary"
-                >
-                  {tag}
+          </Link>
+        )}
+      </CardHeader>
+      <CardContent className="flex flex-col gap-2">
+        <CardTitle>{name}</CardTitle>
+        <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
+          {description}
+        </Markdown>
+      </CardContent>
+      <CardFooter className="flex h-full flex-col items-start justify-between gap-4">
+        {tags && tags.length > 0 && (
+          <div className="mt-2 flex flex-wrap gap-1">
+            {tags.toSorted().map((tag) => (
+              <Badge
+                key={tag}
+                className="px-1 py-0 text-[10px]"
+                variant="secondary"
+              >
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        )}
+        {links && links.length > 0 && (
+          <div className="flex flex-row flex-wrap items-start gap-1">
+            {links.toSorted().map((link, idx) => (
+              <Link href={link?.href} key={idx} target="_blank">
+                <Badge key={idx} className="flex gap-2 px-2 py-1 text-[10px]">
+                  <Icon name={link.icon} className="size-3" />
+                  {link.name}
                 </Badge>
-              ))}
-            </div>
-          )}
-          {links && links.length > 0 && (
-            <div className="flex flex-row flex-wrap items-start gap-1">
-              {links.toSorted().map((link, idx) => (
-                <Link href={link?.href} key={idx} target="_blank">
-                  <Badge key={idx} className="flex gap-2 px-2 py-1 text-[10px]">
-                    <Icon name={link.icon} className="size-3" />
-                    {link.name}
-                  </Badge>
-                </Link>
-              ))}
-            </div>
-          )}
-        </CardFooter>
-      </Card>
+              </Link>
+            ))}
+          </div>
+        )}
+      </CardFooter>
+    </Card>
     </Link>
   );
 }
