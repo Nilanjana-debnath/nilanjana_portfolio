adef7c84ca5c3c081d2569ddb63c6eaf3db673c5
Nilanjana-debnath <nilanjana.debnath24@gmail.com>
2025-03-12 18:36:06 +0530

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-3xl font-bold">
-              ⵣ
+              <img src="/favicon.ico" alt="Nilanjana" width={50} height={50} />
             </Link>
           </div>
           <div className="hidden md:block">
