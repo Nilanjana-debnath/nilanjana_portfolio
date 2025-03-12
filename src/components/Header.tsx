07222278ba73d4e026f3da299065a8608ddcea68
Nilanjana-debnath <nilanjana.debnath24@gmail.com>
2025-03-12 16:39:26 +0530

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
-// import ChatToggle from "./ChatToggle";
+import ChatToggle from "./ChatToggle";
 import ThemeToggle from "./ThemeToggle";
 // import AIChatButton from "./AIChatButton";
+import { Home, Briefcase, Contact, BookOpen, Box, icons } from "lucide-react";
 const navLinks = [
   {
     name: "Home",
     href: "/",
+    icon: Home
   },
   {
     name: "Projects",
     href: "/projects",
+    icon: Briefcase
   },
   {
     name: "Contact",
     href: "/contact",
+    icon: Contact
   },
 ];
 
@@ -24,12 +28,15 @@ export default function Header() {
         <ul className="flex gap-4 sm:gap-8">
           {navLinks.map((nav, id) => (
             <li key={id} className="link">
-              <Link href={nav.href}>{nav.name}</Link>
+              <Link href={nav.href} className="relative px-3 py-2 rounded-md text-sm font-medium   group flex items-center"
+              >
+                  <nav.icon className="w-5 h-5 mr-2" />
+              {nav.name}</Link>
             </li>
           ))}
         </ul>
         <div className="flex gap-0 sm:gap-4">
-          {/* <ChatToggle /> */}
+          <ChatToggle />
           {/* <AIChatButton/> */}
           <ThemeToggle />
         </div>
