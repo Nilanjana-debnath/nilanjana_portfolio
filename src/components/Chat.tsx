df0903630d7906130f7f169906a5efd7a1ca6076
Nilanjana-debnath <nilanjana.debnath24@gmail.com>
2025-03-19 16:00:57 +0530

    'commit'

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -39,7 +39,7 @@ export default function Chat() {
       {/* Chat Interface */}
       <div
         className={`fixed right-8 z-50 transition-all duration-300 ${
-          isVisible ? "bottom-0" : "bottom-0"
+          isVisible ? "bottom-4" : "bottom-4"
         }`}
       >
 
