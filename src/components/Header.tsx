8bb5a2508a6c9a2223e0efb19dbe0d6650256392
Nilanjana-debnath <nilanjana.debnath24@gmail.com>
2025-03-19 15:59:39 +0530

    'commit'

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,22 +16,17 @@ const navLinks = [
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
-  const { theme, resolvedTheme } = useTheme(); // Get the current theme
-  // Determine the logo based on the theme
-  const logoSrc =
-  theme === "dark" || resolvedTheme === "dark"
-    ? "/logo_white.ico"
-    : "/favicon.ico";
+  const { theme } = useTheme(); // Get the current theme
 
   return (
     <header className="sticky top-0 z-50 backdrop-blur-md">
-      <nav className="mx-auto max-w-full px-0 ">
+      <nav className="mx-auto max-w-full px-0">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center">
             <Link href="/" className="text-3xl font-bold">
               <img
-                src={logoSrc} // Change logo based on theme
+                src={theme === "dark" ? "/logo_white.ico" : "/favicon.ico"} // Change logo based on theme
                 alt="Nilanjana"
                 width={40}
                 height={40}
@@ -120,12 +115,12 @@ export default function Header() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.3 }}
         >
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <div className="flex flex-col items-center px-2 pt-2 pb-3 space-y-4 sm:px-3">
             {navLinks.map((link) => (
               <Link
                 key={link.name}
                 href={link.href}
-                className="px-3 py-2 rounded-md text-base font-medium flex items-center"
+                className="px-3 py-2 rounded-md text-base font-medium flex items-center justify-center"
               >
                 <link.icon className="w-5 h-5 mr-2" />
                 {link.name}
